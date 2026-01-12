02b1a1c4f32974465432a7c8da76227c1e81d76e
Math <matthias-merkl@gmx.de>
2026-01-12 23:11:21 +0100

    Changelog: reprojection defines fixes

--- Runtime/ShaderLibrary/TemporalReprojection/Defines.hlsl
+++ Runtime/ShaderLibrary/TemporalReprojection/Defines.hlsl
@@ -4,34 +4,33 @@
     
 #define _TAA_MotionVectorTexture        _CameraMotionVectorsTexture
 #define sampler_TAA_MotionVectorTexture sampler_CameraMotionVectorsTexture
-#define _TAA_Jitter                     _TaaJitter
-#define _TAA_JitterPrev                 _TaaJitterPrev
 
 #elif defined(RAYFORGE_PIPELINE_URP)
     
 #define _TAA_MotionVectorTexture        _MotionVectorTexture
 #define sampler_TAA_MotionVectorTexture sampler_MotionVectorTexture
 
-#if !defined(_TaaJitter)
-#define _TAA_Jitter                 _TAA_Jitter
-#else
-#define _TAA_Jitter                 _TaaJitter
-#endif
-
-#if !defined(_TaaJitterPrev)
-#define _TAA_JitterPrev             _TAA_JitterPrev
-#else
-#define _TAA_JitterPrev             _TaaJitterPrev
-#endif
-
 #else
 
 #define _TAA_MotionVectorTexture        _MotionVectorTexture
 #define sampler_TAA_MotionVectorTexture sampler_MotionVectorTexture
 
-#define _TAA_Jitter                     float2(0.0, 0.0)
-#define _TAA_JitterPrev                 float2(0.0, 0.0)
+#endif
 
+#if defined(_TaaJitter)
+    #define _TAA_Jitter _TaaJitter
+#elif defined(_TAA_Jitter)
+    #define _TAA_Jitter _TAA_Jitter
+#else
+    #define _TAA_Jitter float2(0,0)
+#endif
+
+#if defined(_TaaJitterPrev)
+    #define _TAA_JitterPrev _TaaJitterPrev
+#elif defined(_TAA_JitterPrev)
+    #define _TAA_JitterPrev _TAA_JitterPrev
+#else
+    #define _TAA_JitterPrev float2(0,0)
 #endif
 
 #define _TAA_DepthTexture               _CameraDepthTexture
