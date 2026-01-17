706033e2f8c5f1c2e4df55d3bcd7916b36f4eac3
Math <matthias-merkl@gmx.de>
2026-01-17 08:37:34 +0100

    Changelog: change shader includes to relative paths

--- Runtime/ShaderLibrary/Color/ColorMix.hlsl
+++ Runtime/ShaderLibrary/Color/ColorMix.hlsl
@@ -4,7 +4,7 @@
 // 1. Includes
 // ============================================================================
 
-#include "Packages/com.rayforge.core/Runtime/ShaderLibrary/Common.hlsl"
+#include "../Common.hlsl"
 
 // ============================================================================
 // 2. Utility Functions
